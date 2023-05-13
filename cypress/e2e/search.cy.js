4880aab167031078c7106fa8be3cff7c5636a35d
Ayo-Oluwa <uniqueayo@yahoo.com>
2023-05-13 23:58:59 +0100

    add all files

---
+++ cypress/e2e/search.cy.js
@@ -0,0 +1,39 @@
+describe('Search for books', () => {
+  beforeEach(() => {
+      cy.visit('http://abookstore.azurewebsites.net/')
+  })
+
+  it('should return one book with title Agile Testing', () => {
+      const title = 'Agile Testing'
+      const expectedCount = 1
+
+      // Search
+      cy.get('#searchBar').type(title, { delay: 250 })
+
+      // Verify count
+      cy.get('li:not(.ui-screen-hidden)').should('have.length', expectedCount, `There should be exactly ${expectedCount} book(s) visible`)
+
+      // Verify book title
+      cy.get('h2').should('contain.text', title, `${title} should be visible`)
+  })
+
+  it('should return multiple books with title Test', () => {
+      const title = 'Testi'
+      const expectedBooks = [
+          // 'Test Automation in the Real World',
+          // 'Experiences of Test Automation',
+          // 'Agile Testing',
+          // 'How Google Tests Software',
+          'Java For Testers']
+      const expectedCount = expectedBooks.length
+
+      // Search
+      cy.get('#searchBar').type(title, { delay: 250 })
+
+      // Verify count
+      cy.get('li:not(.ui-screen-hidden)').should('have.length', expectedCount, `There should be exactly ${expectedCount} book(s) visible`)
+
+      // Verify book titles
+      expectedBooks.forEach(b => cy.get('h2').should('contain.text', b, `${b} should be visible`))
+  })
+})
---
+++ cypress/e2e/search_visual.cy.js
@@ -0,0 +1,20 @@
+describe('Search for books - eyes', () => {
+  beforeEach(() => {
+    cy.visit('http://abookstore.azurewebsites.net/')
+    cy.eyesOpen()
+  })
+
+  afterEach(() => {
+    cy.eyesClose()
+  })
+
+  it('eyes should return one book with title Agile Testing', () => {
+    cy.get('#searchBar').type('Agile Testing', {delay:250})
+    cy.eyesCheckWindow()
+  })
+
+  it('eyes should return multiple books with title Test', () => {
+    cy.get('#searchBar').type('Test', {delay:250})
+    cy.eyesCheckWindow()
+  })
+})
