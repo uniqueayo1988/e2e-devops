cdf584e7c0fbdb1ccbcb8d6243000d86613e1b51
Ayo-Oluwa <uniqueayo@yahoo.com>
2023-05-31 16:19:27 +0100

    skip tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://abookstore.azurewebsites.ne/",
+    baseUrl: "https://abookstore.azurewebsites.net/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/search_visual.cy.js
+++ cypress/e2e/search_visual.cy.js
@@ -1,20 +1,20 @@
 describe('Search for books - eyes', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.eyesOpen()
-  })
+  // beforeEach(() => {
+  //   cy.visit('/')
+  //   cy.eyesOpen()
+  // })
 
-  afterEach(() => {
-    cy.eyesClose()
-  })
+  // afterEach(() => {
+  //   cy.eyesClose()
+  // })
 
-  it('eyes should return one book with title Agile Testing', () => {
-    cy.get('#searchBar').type('Agile Testing', {delay:250})
-    cy.eyesCheckWindow()
-  })
+  // it('eyes should return one book with title Agile Testing', () => {
+  //   cy.get('#searchBar').type('Agile Testing', {delay:250})
+  //   cy.eyesCheckWindow()
+  // })
 
-  it('eyes should return multiple books with title Test', () => {
-    cy.get('#searchBar').type('Test', {delay:250})
-    cy.eyesCheckWindow()
-  })
+  // it('eyes should return multiple books with title Test', () => {
+  //   cy.get('#searchBar').type('Test', {delay:250})
+  //   cy.eyesCheckWindow()
+  // })
 })
